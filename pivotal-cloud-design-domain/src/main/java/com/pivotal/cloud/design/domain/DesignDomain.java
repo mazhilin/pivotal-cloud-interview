290ff82377b6b9ce0f51c6baa02babe809eef5a6
marklin <marklin1992@outlook.com>
2020-05-19 21:12:10 +0800

    master{2020-05-19 星期二]:commit to master

---
+++ pivotal-cloud-design-domain/src/main/java/com/pivotal/cloud/design/domain/DesignDomain.java
@@ -0,0 +1,14 @@
+package com.pivotal.cloud.design.domain;
+
+/**
+ * @className: com.pivotal.cloud.design.domain.DesignDomain
+ * @title: DesignDomain
+ * @description: 封装Pivotal项目DesignDomain类
+ * @content: //TODO
+ * @author: marklin
+ * @datetime: 2020-05-19 21:10
+ * @version: 1.0.0
+ * @copyright: Copyright © 2018-2020 Pivotal Systems Incorporated. All rights reserved.
+ */
+public interface DesignDomain {
+}
---
+++ pivotal-cloud-design-structure/src/main/java/com/pivotal/cloud/design/structure/DesignStructure.java
@@ -0,0 +1,14 @@
+package com.pivotal.cloud.design.structure;
+
+/**
+ * @className: com.pivotal.cloud.design.structure.DesignStructure
+ * @title: DesignStructure
+ * @description: 封装Pivotal项目DesignStructure类
+ * @content: //TODO
+ * @author: marklin
+ * @datetime: 2020-05-19 21:11
+ * @version: 1.0.0
+ * @copyright: Copyright © 2018-2020 Pivotal Systems Incorporated. All rights reserved.
+ */
+public interface DesignStructure {
+}
